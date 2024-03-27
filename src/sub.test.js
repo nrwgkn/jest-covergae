4f7a7ff605a89af5493ba1901a71ed3b42d8f789
nrwgkn <rino.nishimura@zozo.com>
2024-03-27 15:13:56 +0900

    add test

--- src/sub.test.js
+++ src/sub.test.js
@@ -3,3 +3,7 @@ const sub = require("./sub");
 test("adds 2 - 1 to equal 1", () => {
   expect(sub(2, 1)).toBe(1);
 });
+
+test("adds 1 - 100 to equal 0", () => {
+  expect(sub(1, 100)).toBe(0);
+});
