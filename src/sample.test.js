49c2dd4908ee2ce485a3fd24a5c71198bcbb3860
nrwgkn <rino.nishimura@zozo.com>
2024-03-29 19:00:52 +0900

    fix

--- src/sample.test.js
+++ src/sample.test.js
@@ -3,3 +3,11 @@ const sample = require("./sample");
 test("sample", () => {
   expect(sample(1)).toBe("sample1");
 });
+
+test("sample", () => {
+  expect(sample(2)).toBe("sample2");
+});
+
+test("sample", () => {
+  expect(sample(0)).toBe("sample0");
+});
