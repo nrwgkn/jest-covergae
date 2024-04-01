773c6487ad5c6db0be7db3482f63e5b0d68f2e78
nrwgkn <rino.nishimura@zozo.com>
2024-04-01 14:27:23 +0900

    feat

--- src/sub.test.js
+++ src/sub.test.js
@@ -3,3 +3,7 @@ const sub = require("./sub");
 test("adds 2 - 1 to equal 1", () => {
   expect(sub(2, 1)).toBe(1);
 });
+
+test("subtracts 1 - 2 to equal -1", () => {
+  expect(sub(1, 2)).toBe(-1);
+});
